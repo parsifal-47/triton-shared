c13e6cb2f06960bc82ba7c45111101f007cb47e7
Renat Idrisov <parsifal-47@users.noreply.github.com>
2024-08-03 09:04:26 -0700

    Trying

--- python/examples/conftest.py
+++ python/examples/conftest.py
@@ -6,11 +6,71 @@ from triton.backends.triton_shared.driver import CPUDriver
 
 triton.runtime.driver.set_active(CPUDriver())
 
+
 def empty_decorator(func):
     return func
 
 pytest.mark.interpreter = empty_decorator
 
+
 @pytest.fixture
 def device(request):
     return "cpu"
+
+
+tests_not_supported = {
+    "test_compare_op",
+    "test_slice",
+    "test_where",
+    "test_math_erf_op",
+    "test_precise_math",
+    "test_abs_fp8",
+    "test_shapes_as_params",
+    "test_transpose",
+    "test_noinline",
+    "test_atomic_rmw",
+    "test_tensor_atomic_rmw",
+    "test_atomic_cas",
+    "test_tensor_atomic_cas",
+    "test_cast",
+    "test_cat",
+    "test_store_constant",
+    "test_reduce1d",
+    "test_scan2d",
+    "test_histogram",
+    "test_optimize_thread_locality",
+    "test_scan_layouts",
+    "test_reduce_layouts",
+    "test_store_op",
+    "test_convert1d",
+    "test_chain_reduce",
+    "test_generic_reduction",
+    "test_trans_4d",
+    "test_dot",
+    "test_dot3d",
+    "test_constexpr",
+    "test_arange",
+    "test_masked_load",
+    "test_reshape",
+    "test_trans_reshape",
+    "test_if",
+    "test_if_call",
+    "test_convert2d",
+    "test_convertmma2mma",
+    "test_dot_max_num_imprecise_acc",
+    "test_propagate_nan",
+    "test_clamp",
+    "test_clamp_symmetric",
+    "test_temp_var_in_loop",
+    "test_math_extern"
+}
+
+
+def pytest_collection_modifyitems(config, items):
+    skip_marker = pytest.mark.skip(reason="CPU backend does not support it yet")
+
+    for item in items:
+        test_func_name = item.originalname if item.originalname else item.name
+        
+        if test_func_name in tests_not_supported:
+            item.add_marker(skip_marker)
