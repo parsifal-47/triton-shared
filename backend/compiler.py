119d6070a7450b5e0bc9fa8dd292dbcd52c32334
Renat Idrisov <parsifal-47@users.noreply.github.com>
2024-08-07 14:22:20 -0700

    Trying

--- backend/compiler.py
+++ backend/compiler.py
@@ -32,7 +32,10 @@ def _ttir_to_ttsharedir(mod):
         dst_path = os.path.join(tmpdir, "ttshared.mlir")
         Path(src_path).write_text(ttir_code)
         triton_shared_opt_path = _get_triton_shared_opt_path()
-        subprocess.check_call([triton_shared_opt_path, src_path, "--triton-to-linalg-experimental", "-o", dst_path])
+        subprocess.check_call([triton_shared_opt_path, src_path,
+            "--triton-to-linalg-experimental",
+            "--linalg-to-linear-algebra-subprograms",
+            "-o", dst_path])
         return Path(dst_path).read_text()
 
 
