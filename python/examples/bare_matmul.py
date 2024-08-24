d53e5b3225987239a6e8bbba61b98494e3b89e56
Renat Idrisov <parsifal-47@users.noreply.github.com>
2024-08-23 17:05:22 -0700

    Trying

--- python/examples/bare_matmul.py
+++ python/examples/bare_matmul.py
@@ -7,22 +7,17 @@ import benchmark
 
 @triton.jit
 def bare_matmul(X, Y, Z, M, N, K, BLOCK_SIZE: tl.constexpr):
-    # Get the program IDs for the current block
     pid_x = tl.program_id(0)  # block row id
     pid_y = tl.program_id(1)  # block column id
 
-    # Define offsets for loading submatrices (blocks)
     offs_x = pid_x * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
     offs_y = pid_y * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
 
-    # Load blocks (submatrices) from global memory
     x = tl.load(X + offs_x[:, None] * K + offs_y[None, :])
     y = tl.load(Y + offs_x[:, None] * N + offs_y[None, :])
 
-    # Perform the dot product
     z = tl.dot(x, y)
 
-    # Store the result
     tl.store(Z + offs_x[:, None] * N + offs_y[None, :], z)
 
 
