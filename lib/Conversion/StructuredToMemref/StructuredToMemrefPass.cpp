4579bafb94c441305eee0c6a3b103b7ab30767e1
Renat Idrisov <parsifal-47@users.noreply.github.com>
2024-09-01 12:30:23 -0700

    Trying

--- lib/Conversion/StructuredToMemref/StructuredToMemrefPass.cpp
+++ lib/Conversion/StructuredToMemref/StructuredToMemrefPass.cpp
@@ -27,6 +27,7 @@
 #include "mlir/Dialect/SCF/Transforms/Patterns.h"
 #include "mlir/Dialect/Tensor/TransformOps/TensorTransformOps.h"
 #include "mlir/Dialect/Transform/IR/TransformDialect.h"
+#include "mlir/Dialect/Tensor/Transforms/Transforms.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/Passes.h"
 #include "triton/Dialect/Triton/IR/Types.h"
